1bf1743165786d715e97eb8edb246f910d707a0f
archastronaut-hypr <gericapo98@yahoo.com>
2024-12-22 12:45:46 +0100

    more abstraction: added get functions for states and transitions

--- go-fun/basics/deadlocks_using_interfaces.go
+++ go-fun/basics/deadlocks_using_interfaces.go
@@ -29,12 +29,15 @@ type BisimulationCheck interface {
   CheckBisimulation(LTS,LTS) bool
 }
 // https://dev.to/envitab/function-signatures-in-go-38ja
+// 
 type ltsImpl struct {
 	states        []State
 	transitions   []Transition
 	initialStates []State
 }
-
+// https://www.digitalocean.com/community/tutorials/how-to-use-variadic-functions-in-go
+// The initial states are designated as possible traversing points after creating the lts
+// Will be changed accordingly...
 func NewLTS(initial ...State) LTS {
 	return &ltsImpl{
 		states:        []State{},
@@ -42,6 +45,20 @@ func NewLTS(initial ...State) LTS {
 		initialStates: initial,
 	}
 }
+func (l *ltsImpl) States() []State {
+	return l.states
+}
+
+func (l *ltsImpl) Transitions() []Transition {
+	return l.transitions
+}
+
+/*
+* TODO::
+* Now that we have a framework for lts.
+* We need to be able to add states passed variadic parameter 
+* into the array of State which should be empty in the beginning.
+*/
 
 type deadlock_checker bool {
   
