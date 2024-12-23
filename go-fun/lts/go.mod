c64d7a1be18a170d6e7ce2e9eab264204e86c7cc
archastronaut-hypr <gericapo98@yahoo.com>
2024-12-23 18:56:55 +0100

    lts bfs search test scenario 1

---
+++ go-fun/lts/go.mod
@@ -0,0 +1,3 @@
+module github.com/gericapo/codelab/lts
+
+go 1.23.4
---
+++ go-fun/lts/lts_lib.go
@@ -0,0 +1,128 @@
+package lts_lib
+
+import (
+  "container/list"
+  "errors"
+  "fmt"
+  "strings"
+)
+
+type State interface {
+  ID() string
+}
+
+type Transitions interface {
+  Source() State 
+  Label() string
+  Destination() State
+}
+
+type LTS interface {
+  States() []State 
+  Transitions() []Transition
+  InitialStates() []State 
+  AddState(State)
+  AddTransition(Transition)
+  AddStates(...State) error 
+  AddTransitions(...Transition) error 
+}
+
+type BisimulationCheck interface {
+  CheckBisimulation(LTS,LTS) bool
+}
+// https://dev.to/envitab/function-signatures-in-go-38ja
+// 
+type ltsImpl struct {
+	states        []State
+	transitions   []Transition
+	initialStates []State
+}
+// https://www.digitalocean.com/community/tutorials/how-to-use-variadic-functions-in-go
+// The initial states are designated as possible traversing points after creating the lts
+// Will be changed accordingly...
+func NewLTS(initial ...State) LTS {
+	return &ltsImpl{
+		states:        make([]State, 0),
+		transitions:   make([]Transition,0),
+		initialStates: initial,
+	}
+}
+func (l *ltsImpl) States() []State {
+	return l.states
+}
+
+func (l *ltsImpl) Transitions() []Transition {
+	return l.transitions
+}
+
+/*
+* TODO::
+* Now that we have a framework for lts.
+* We need to be able to add states passed as variadic parameter 
+* into the array of State which should be empty in the beginning.
+*/
+
+// a function that adds states to a lts should anticipate duplication and,
+// skip any such occurrence
+//
+// Update: implement this in two functions. 
+func (l *ltsImpl) AddState(s *State) error {
+	for _, existing := range l.states {
+		if existing.ID() == s.ID() {
+			return errors.New("duplicate state ID: " + s.ID())
+		}
+	}
+	l.states = append(l.states, s)
+	return nil
+}
+// adding multiple states using variadic parameter
+func (l *ltsImpl) AddStates(sts ...State) error {
+	for _, s := range sts {
+		if err := l.AddState(s); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+// only passing the value of the transition interface,
+// not sure yet where to point for methods arguments
+func (l *ltsImpl) AddTransition(t *Transition) error {
+	l.transitions = append(l.transitions, t)
+	return nil
+}
+// be aware 
+func (l *ltsImpl) AddTransitions(ts ...Transition) error {
+	for _, t := range ts {
+		if err := l.AddTransition(t); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func TraverseAndPrintBFS(l LTS, start State) {
+	visited := make(map[string]bool)
+	queue := []State{start}
+	visited[start.ID()] = true
+
+	fmt.Printf("BFS traversal beginning at state: %s\n", start.ID())
+	
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		fmt.Printf(" - Visited state: %s\n", current.ID())
+		
+		for _, tr := range l.Transitions() {
+			if tr.Source().ID() == current.ID() {
+				fmt.Printf("    via '%s' -> %s\n", tr.Label(), tr.Destination().ID())
+				
+				if !visited[tr.Destination().ID()] {
+					visited[tr.Destination().ID()] = true
+					queue = append(queue, tr.Destination())
+				}
+			}
+		}
+	}
+}
+
