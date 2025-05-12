b40f64ffdc5335bc4f85bd61b6b9e22e6427540c
archastronaut-hypr <gericapo98@yahoo.com>
2025-05-13 01:03:10 +0200

    lists

--- python-fun/python-for-beginners/loops.py
+++ python-fun/python-for-beginners/loops.py
@@ -34,3 +34,20 @@ total = 0
 for number in numbers:
     total += number
 print(total)
+
+# ---------------------------------------- #
+# nested loops
+for x in range(4):
+    for y in range(3):
+        print(f'({x}, {y})')
+# ---------------------------------------- #
+
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
+
+# ---------------------------------------- #
+
