6d3e128dceb37b51201675dc041c2061feb0b747
archastronaut-hypr <gericapo98@yahoo.com>
2025-05-13 04:56:58 +0200

    hmm

--- python-fun/python-for-beginners/tuples.py
+++ python-fun/python-for-beginners/tuples.py
@@ -2,5 +2,11 @@ numbers = (1, 2, 3)
 #numbers[0] = 10
 #print(numbers[0])
 
-
 print(numbers[0] * numbers[1] * numbers[2])
+
+cordinates = (1, 2, 3)
+x, y, z = cordinates 
+print(x)
+print(y)
+print(z)
+
